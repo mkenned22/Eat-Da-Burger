2e01e3282307f13273e7db336bd48fe3d51a6f25
Mike Kennedy <mkenned22@gmail.com>
2018-05-27 21:42:46 -0400

    adding in JAWS DB

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,20 @@
 var mysql = require("mysql");
 require('dotenv').config();
 
+var connection;
 // Create connection object
-var connection = mysql.createConnection({
-    host: process.env.DB_host,
-    user: process.env.DB_user,
-    password: process.env.DB_password,
-    database: process.env.DB_database
-})
+
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else{
+    connection = mysql.createConnection({
+        host: process.env.DB_host,
+        user: process.env.DB_user,
+        password: process.env.DB_password,
+        database: process.env.DB_database
+    });
+}
 
 // Open connection
 connection.connect(function(error){
